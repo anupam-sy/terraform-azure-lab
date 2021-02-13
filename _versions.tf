58d8b33ba2d6cda69ce29cb5b8615c9831f20673
anupam-sy <anupam.yaadav@gmail.com>
2021-02-13 22:25:02 +0530

    [ADD]: Terraform configuration templates added.

---
+++ _versions.tf
@@ -0,0 +1,11 @@
+# Terraform block to configure Terraform and Provider Version
+terraform {
+  required_version = "~> 0.14.6" # To allow upgrade of 0.14.x versions.
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
