348b27f2838b59c2b03f89ea78f9d6783c83359b
anupam-sy <anupam.yaadav@gmail.com>
2021-02-13 22:26:40 +0530

    [ADD]: Variable declaration files added.

---
+++ resources.tfvars
@@ -0,0 +1,13 @@
+# Variable Values - RG, Location
+rg_name  = "FC-TST-RG-01"
+location = "UK South"
+rs_tags = {
+  "Project"     = "FC"
+  "Environment" = "TST"
+}
+
+# Variable Values - IaaS Resources
+avset_names = ["FC-TST-AST-01", "FC-TST-AST-02"]
+
+# Variable Values - PaaS Resources
+appsp_names = ["FC-TST-ASP-01", "FC-TST-ASP-02"]
---
+++ variables.tf
@@ -0,0 +1,30 @@
+# Authentication Specific Variables
+variable "azure_subscription_id" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
+
+# Variable Definition - RG, Location
+variable "rg_name" {
+  description = "Azure Resouce Group Name"
+}
+
+variable "location" {
+  description = "Azure Resouce Group Location"
+  default     = "UK South"
+}
+
+variable "rs_tags" {
+  description = "Azure Resource Tags"
+  default = {
+    "Project"     = "FC"
+    "Environment" = "TST"
+  }
+}
+
+# Variable Definition - IaaS Resources
+variable "avset_names" {}
+
+# Variable Definition - PaaS Resources
+variable "appsp_names" {}
+
